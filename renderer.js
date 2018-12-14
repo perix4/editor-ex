93beea9ce3a97652f39b69b7a57136c920acf582
Petar Kujundzic <petar.kujundzic@limun-studio.hr>
2018-12-14 11:07:41 +0100

    adds flexbox

--- renderer.js
+++ renderer.js
@@ -5,24 +5,24 @@ const code = () => document.getElementsByName("code")[0];
 const output = () => document.getElementsByName("output")[0];
 
 const editor = ace.edit(code(), {
-  mode: "ace/mode/elixir",
-  selectionStyle: "text",
-  theme: "ace/theme/tomorrow_night"
+    mode: "ace/mode/elixir",
+    selectionStyle: "text",
+    theme: "ace/theme/tomorrow_night"
 });
 
-document.onkeydown = function (evt) {
-  evt = evt || window.event;
-  if (evt.ctrlKey && evt.keyCode == 13) {
-    ipcRenderer.send('form-submission', { input: input().innerText, code: editor.getValue() });
-  }
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    if (evt.ctrlKey && evt.keyCode == 13) {
+        ipcRenderer.send('form-submission', { input: input().innerText, code: editor.getValue() });
+    }
 };
 
-ipcRenderer.on('result', function (event, result) {
-  if (result.err) {
-    output().innerHTML = result.stderr;
-    output().classList.add("error");
-  } else {
-    output().innerHTML = result.stdout;
-    output().classList.remove("error");
-  }
+ipcRenderer.on('result', function(event, result) {
+    if (result.err) {
+        output().innerHTML = result.stderr;
+        output().classList.add("error");
+    } else {
+        output().innerHTML = result.stdout;
+        output().classList.remove("error");
+    }
 });
